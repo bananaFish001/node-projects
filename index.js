d9bea230934a5fef6ee87c620690d3dfeb417e7a
bananaFish001 <raj123.rs308@gmail.com>
2024-04-28 22:19:29 +0530

    fun test app for backend

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Chai aur code')
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/reddit', (req, res) => {
+    res.send('bananaFish')
+})
+
+app.get('/chainsaw', (req, res) => {
+    res.send('<h1>Please watch legend of the galactic heroes</h1>')
+})
+
+app.get('/coffee', (req, res) => {
+    res.send('coffee is the elixr of life')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+    console.log(`After changes new port is ${process.env.PORT}`)
+})
